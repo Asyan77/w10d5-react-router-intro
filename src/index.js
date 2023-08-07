413d4b11aa3136ce5520cbfd6ea063ad8a797e09
Ashley <asyan77@gmail.com>
2023-08-06 18:11:35 -0700

    finish project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// const root = ReactDOM.createElement(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//       <Root />
+//   </React.StrictMode>,
+// )
